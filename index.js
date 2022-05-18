258d69ae0017965c03f525ff0d617fec6e88b1be
Martin Contreras <martincdh92@gmail.com>
2022-05-18 18:04:50 -0500

    Adding keypress event and animations

--- index.js
+++ index.js
@@ -3,36 +3,55 @@ var buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
     button.addEventListener("click", function () {
-        switch (this.innerHTML) {
-            case 'w':
-                var audio = new Audio('sounds/crash.mp3');
-                audio.play();
-                break;
-            case 'a':
-                var audio = new Audio('sounds/kick-bass.mp3');
-                audio.play();
-                break;
-            case 's':
-                var audio = new Audio('sounds/snare.mp3');
-                audio.play();
-                break;
-            case 'd':
-                var audio = new Audio('sounds/tom-1.mp3');
-                audio.play();
-            case 'j':
-                var audio = new Audio('sounds/tom-2.mp3');
-                audio.play();
-                break;
-            case 'k':
-                var audio = new Audio('sounds/tom-3.mp3');
-                audio.play();
-                break;
-            case 'l':
-                var audio = new Audio('sounds/tom-4.mp3');
-                audio.play();
-                break;
-            default:
-                break;
-        }
+        makeSound(this.innerHTML);
+        animateButton(this.innerHTML);
     });
 });
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    animateButton(event.key);
+});
+
+function makeSound(character) {
+    switch (character) {
+        case 'w':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+        case 'j':
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function animateButton(character) {
+    var clickedButton = document.querySelector("." + character);
+    clickedButton.classList.add("pressed");
+    
+    setTimeout(function() {
+        clickedButton.classList.remove("pressed");
+    }, 150);
+}
